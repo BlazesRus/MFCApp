6373da2b415c2741879366cab9782ae49d534393
BlazesRus (James) <BlazesRus@yahoo.com>
2021-10-18 00:04:10 -0400

    Code for retrieving preprocessor file from https://stackoverflow.com/questions/30438911/how-to-get-a-visual-studio-macro-value-into-a-pre-processor-directive

--- MFCpch.h
+++ MFCpch.h
@@ -1,4 +1,4 @@
-// MFCpch.h: This is a precompiled header file.
+// This is a precompiled header file.
 // Files listed below are compiled only once, improving build performance for future builds.
 // This also affects IntelliSense performance, including code completion and many code browsing features.
 // However, files listed here are ALL re-compiled if any one of them is updated between builds.
@@ -7,7 +7,14 @@
 #ifndef PCH_H
 #define PCH_H
 
-// add headers that you want to pre-compile here
+//Generating Precompiled header name based on Macro based on code from
+//https://stackoverflow.com/questions/30438911/how-to-get-a-visual-studio-macro-value-into-a-pre-processor-directive
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#define MfcPrecompileFile   TOSTRING(MfcPCHFile)//Referencing MFCpch.h file
+
+// add headers that you want to be precompiled here
 #include "MFCFramework.h"
 
 #endif //PCH_H
